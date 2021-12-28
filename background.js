c47ed243ea880a0fb310730fedb1fd2bbf6386a5
gonudayo <kgw010119@gmail.com>
2021-12-28 14:19:00 +0000

    feat: Time Limit Alert
    Set time limit - use  range slider input

--- background.js
+++ background.js
@@ -1,41 +1,57 @@
-let TIME = 0;
+let TIME = 0; // 타이머 시간
+let limit = 30; // 시간 제한
 let cron; // clearInterval을 위한 변수
 
 function startButton() {
   updateTimer();
   stopButton();
   cron = setInterval(updateTimer, 1000);
-  // timer.classList.add('hide');
 }
 
 function stopButton() {
   clearInterval(cron);
-  // timer.classList.remove('hide');
+}
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
 function updateTimer(){
-    TIME++;
-    console.log(TIME)
-    chrome.runtime.sendMessage({
-      msg: TIME
+  TIME++;
+  const checkMinutes = Math.floor(TIME/60); 
+  const minutes = checkMinutes % 60;
+
+  // 시간 초과시 stop 메시지 발송
+  if(minutes >= limit) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var currTab = tabs[0];
+      if (currTab !== undefined) { 
+        console.log(currTab)
+        chrome.tabs.sendMessage( currTab.id, {
+          message: 'stop'
+        })
+      }
+    });
+  }
+
+  // popup의 limit값 받아오기
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        limit = request.msg;
+    }
+  );
+  
+  //popup에 time과 limit값 보내기
+  chrome.runtime.sendMessage({
+    msg: 'success',
+    data: {
+      time: TIME,
+      limit: limit
+    }
   });
 }
-
-
-
-chrome.tabs.onUpdated.addListener(
-  function(tabId, info, tab) {
-    // read info data and do something with it
-    // like send the new url to contentscripts.js
-    if (info.url) {
-      // 현재 탭으로 url 전송
-      chrome.tabs.sendMessage( tabId, {
-        message: 'hello!',
-        url: info.url
-      })
-    }
-  }
-);
 
 // 시작할 때
 chrome.windows.getAll({populate:true},function(windows){
@@ -55,35 +71,33 @@ chrome.windows.getAll({populate:true},function(windows){
 // tab을 열거나 업데이트 될 때
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
   let youtube = 0;
-      chrome.windows.getAll({populate:true},function(windows){
-        windows.forEach(function(window){
-          window.tabs.forEach(function(tab){
-            if(tab.url.indexOf('https://www.youtube.com/') !== -1) {
-              youtube += 1;
-              if(youtube === 1) startButton();
-            }
-            console.log(tab.url);
-          });
-        });
+  chrome.windows.getAll({populate:true},function(windows){
+    windows.forEach(function(window){
+      window.tabs.forEach(function(tab){
+        if(tab.url.indexOf('https://www.youtube.com/') !== -1) {
+          youtube += 1;
+          if(youtube === 1) startButton();
+        }
+        console.log(tab.url);
       });
-    if(youtube === 0) stopButton();
+    });
+  });
+  if(youtube === 0) stopButton();
 });
 
 // tab을 닫을 때
 chrome.tabs.onRemoved.addListener(function(tabId, info) {
   let youtube = 0;
-      chrome.windows.getAll({populate:true},function(windows){
-        windows.forEach(function(window){
-          window.tabs.forEach(function(tab){
-            if(tab.url.indexOf('https://www.youtube.com/') !== -1) {
-              youtube += 1;
-              if(youtube === 1) startButton();
-            }
-            console.log(tab.url);
-          });
-        });
+  chrome.windows.getAll({populate:true},function(windows){
+    windows.forEach(function(window){
+      window.tabs.forEach(function(tab){
+        if(tab.url.indexOf('https://www.youtube.com/') !== -1) {
+          youtube += 1;
+          if(youtube === 1) startButton();
+        }
+        console.log(tab.url);
       });
+    });
+  });
   if(youtube === 0) stopButton();
 });
-
-
--- popup.js
+++ popup.js
@@ -1,15 +1,29 @@
 const timer = document.querySelector('.js-timer');
+let limit = 0;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        let TIME = request.msg;
+        let TIME = request.data.time;
         const hours = Math.floor(TIME/3600);
         const checkMinutes = Math.floor(TIME/60); 
         const seconds = TIME % 60;
         const minutes = checkMinutes % 60;
-        
+        limit = request.data.limit;
+        output.innerHTML = limit;
+        slider.value = limit;
         timer.innerText = `${hours < 10 ? `0${hours}` : hours}:${
             minutes < 10 ? `0${minutes}` : minutes}:${
             seconds < 10 ? `0${seconds}` : seconds}`;
     }
 );
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = limit;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    chrome.runtime.sendMessage({
+        msg: this.value
+    });
+}
