80b9b45e02616a8129b9d984dfd463cee3136617
gonudayo <kgw010119@gmail.com>
2021-12-31 11:33:43 +0000

    feat: Movement Method Change on Option
    add icon

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 let TIME = 0; // 타이머 시간
 let limit = 30; // 시간 제한
 let cron; // clearInterval을 위한 변수
+let movementMethod = 1; // 동작 설정
 
 function startButton() {
   updateTimer();
@@ -29,23 +30,31 @@ function updateTimer() {
   const checkMinutes = Math.floor(TIME / 60);
   const minutes = checkMinutes % 60;
 
-  // 시간 초과시 YouTube 종료
-  if (minutes > limit) {
-    chrome.windows.getAll({ populate: true }, function (windows) {
-      windows.forEach(function (window) {
-        window.tabs.forEach(function (tab) {
-          if (tab.url.indexOf("https://www.youtube.com/") !== -1) {
-            chrome.tabs.remove(tab.id, function () {});
-          }
+  // 시간 초과시 YouTube 종료 혹은 경고 메시지
+  if (TIME > limit) {
+    if (movementMethod === 1) {
+      chrome.windows.getAll({ populate: true }, function (windows) {
+        windows.forEach(function (window) {
+          window.tabs.forEach(function (tab) {
+            if (tab.url.indexOf("https://www.youtube.com/") !== -1) {
+              chrome.tabs.remove(tab.id, function () {});
+            }
+          });
         });
       });
-    });
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var currTab = tabs[0];
-      chrome.tabs.sendMessage(currTab.id, {
-        message: "유튜브 그만 봐!",
+    } else if (movementMethod === 2) {
+      chrome.windows.getAll({ populate: true }, function (windows) {
+        windows.forEach(function (window) {
+          window.tabs.forEach(function (tab) {
+            if (tab.url.indexOf("https://www.youtube.com/") !== -1) {
+              chrome.tabs.sendMessage(tab.id, {
+                message: "유튜브 그만 봐!",
+              });
+            }
+          });
+        });
       });
-    });
+    }
   }
 }
 
@@ -69,6 +78,10 @@ chrome.runtime.onConnect.addListener(function (port) {
     }
   });
 
+  chrome.runtime.sendMessage({
+    movementMethod: movementMethod,
+  });
+
   // 옵션 받아오기
   chrome.runtime.onMessage.addListener(function (
     request,
@@ -77,6 +90,8 @@ chrome.runtime.onConnect.addListener(function (port) {
   ) {
     if (request.reset) {
       TIME = 0;
+    } else if (request.movementMethod) {
+      movementMethod = request.movementMethod;
     }
   });
 });
@@ -113,6 +128,9 @@ chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
 
 // tab을 닫을 때
 chrome.tabs.onRemoved.addListener(function (tabId, info) {
+  chrome.runtime.sendMessage({
+    movementMethod: movementMethod,
+  });
   let youtube = 0;
   chrome.windows.getAll({ populate: true }, function (windows) {
     windows.forEach(function (window) {
--- options.js
+++ options.js
@@ -1,5 +1,30 @@
 chrome.runtime.connect({ name: "option" });
 
+let movementMethod = 0;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.movementMethod) {
+    if (request.movementMethod === 1)
+      document.getElementById("autoClose").checked = true;
+    else if (request.movementMethod === 2)
+      document.getElementById("justAlert").checked = true;
+  }
+});
+
+document.getElementById("autoClose").addEventListener("click", function () {
+  movementMethod = 1;
+  chrome.runtime.sendMessage({
+    movementMethod: 1,
+  });
+});
+
+document.getElementById("justAlert").addEventListener("click", function () {
+  movementMethod = 2;
+  chrome.runtime.sendMessage({
+    movementMethod: 2,
+  });
+});
+
 document.getElementById("btn").addEventListener("click", function () {
   chrome.runtime.sendMessage({
     reset: 1,
